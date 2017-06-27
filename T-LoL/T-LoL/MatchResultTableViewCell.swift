96878b3f51d0e3fac52a2ffaaea91be57cd080aa
Daeyun Ethan Kim <nicebuvr@naver.com>
2017-06-27 15:39:32 +0900

    [MatchResultTableViewCell] 게임 리스트 테이블 뷰 셀 추가

---
+++ T-LoL/T-LoL/MatchResultTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MatchResultTableViewCell.swift
+//  T-LoL
+//
+//  Created by Daeyun Ethan Kim on 27/06/2017.
+//  Copyright © 2017 Daeyun Ethan Kim. All rights reserved.
+//
+
+import UIKit
+
+class MatchResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
